afa2e556d88030bf80c56e4e4dca9f30e1544d80
Salaudeen Abdulbaki <sabdulba@c3r5p1.hive.fi>
2024-10-31 12:21:15 +0200

    added split

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,11 +6,27 @@
 /*   By: sabdulba <sabdulba@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/30 11:33:09 by sabdulba          #+#    #+#             */
-/*   Updated: 2024/10/30 12:52:54 by sabdulba         ###   ########.fr       */
+/*   Updated: 2024/10/31 12:01:25 by sabdulba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+/*
+CALLOC(3) (simplified)
+
+NAME
+    calloc -- memory allocation
+SYNOPSIS
+    void *calloc(size_t count, size_t size);
+DESCRIPTION
+    The calloc() function allocates memory.
+    The allocated memory is aligned such that it can be used for any data type.
+    The calloc() function contigously allocates enough space for count objects that are size bytes of memory each and returns a pointer to the allocated memory.
+    The allocated memory is filled with bytes of value zero.
+RETURN VALUES
+    If successful, calloc() returns a pointer to allocated memory. If there is an error, they return a NULL pointer and set errno to ENOMEM.
+*/
 
 void *ft_calloc(size_t count, size_t size)
 {
--- ft_split.c
+++ ft_split.c
@@ -6,60 +6,85 @@
 /*   By: sabdulba <sabdulba@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/30 17:52:27 by sabdulba          #+#    #+#             */
-/*   Updated: 2024/10/30 18:30:19 by sabdulba         ###   ########.fr       */
+/*   Updated: 2024/10/31 12:16:53 by sabdulba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int	countDelimiter(char const *s, char c);
+static char *copyWords (char const *start, char const *end);
+static int	countWord(char const *s, char c);
 
 char **ft_split(char const *s, char c)
 {
 	int i;
-	int j;
-	
+	char const *start;
 	char **splitted;
-	//int c_num;
-	int splitLen;
+	int wordCount;
+	char *perString;
 
-	splitLen = countDelimiter(s, c);
-	splitted = (char **)malloc(sizeof(char *) * (splitLen + 1));
+	if(!s)
+		return (NULL);
+	wordCount = countWord(s, c);
+	splitted = (char **)malloc((wordCount + 1) * sizeof(char *));
 	if (!splitted)
 		return (NULL);
 	i = 0;
-	while(splitted[i])
+	while(*s != '\0')
 	{
-		j = 0;
-		while(*s != 0)
+		if(*s != c)
 		{
-			if(*s != c)
-			{
-				splitted[i][j] = *s;
-				j++;
+			start = s;
+			while (*s && *s != c)
 				s++;
-			}
-			else
-			{
-				break;
-			}
+			perString = copyWords(start, s);
+			splitted[i] = perString;
+			i++;
 		}
-		i++;
+		s++;
 	}
-	splitted[i][j] = '\0';
+	free(perString);
+	splitted[i] = NULL;
 	return (splitted);
 }
-static int	countDelimiter(char const *s, char c)
+static char	*copyWords(char const *start, char const *end)
+{
+	int length;
+	char *dest;
+	char *string;
+
+	length = end - start;
+	string = (char *)malloc(length + 1);
+	if (!string)
+		return(NULL);
+	dest = string;
+	while (start < end)
+	{
+		*dest++ = *start++;
+	}
+	*dest = '\0';
+	return (string);
+}
+
+//helper to count number of word strings seperated by the delimiter
+static int	countWord(char const *s, char c)
 {
 	int	i;
 	int	count;
+	int inWord;
 	
 	i = 0;
 	count = 0;
+	inWord = 0; //like a boolean to figure out if we are in a word or not.
 	while (s[i] != 0)
 	{
 		if (c == s[i])
+			inWord = 0;
+		if(!inWord && c != s[i])
+		{
 			count++;
+			inWord = 1;
+		}
 		i++;
 	}
 	return (count);
